a5dfa656a940d3dfe4f25666d79c0d8255efc608
Akul Pillai <contact@akulpillai.com>
2019-05-30 00:50:51 +0530

    Prepare directory structure for pkgsrc

--- qemu_mode/build_qemu_support.sh
+++ qemu_mode/build_qemu_support.sh
@@ -5,14 +5,9 @@
 
 cd qemu
 
-CFLAGS="-O3" ./configure --disable-werror \
-  --enable-system --enable-linux-user \
-  --enable-guest-base --disable-gtk --disable-sdl --disable-vnc \
-  --target-list="x86_64-linux-user x86_64-softmmu arm-softmmu aarch64-softmmu"
-
 make
-cp -f "x86_64-linux-user/qemu-x86_64" "../../afl-qemu-trace"
-cp -f "x86_64-softmmu/qemu-system-x86_64" "../../afl-qemu-system-trace"
-cp -f "x86_64-softmmu/qemu-system-x86_64" "../../qemu-system-x86_64"
-cp -f "arm-softmmu/qemu-system-arm" "../../qemu-system-arm"
-cp -f "aarch64-softmmu/qemu-system-aarch64" "../../qemu-system-aarch64"
+cp -f "work/qemu-2.3.0/x86_64-bsd-user/qemu-x86_64" "../../afl-qemu-trace"
+cp -f "work/qemu-2.3.0/x86_64-softmmu/qemu-system-x86_64" "../../afl-qemu-system-trace"
+cp -f "work/qemu-2.3.0/x86_64-softmmu/qemu-system-x86_64" "../../qemu-system-x86_64"
+cp -f "work/qemu-2.3.0/arm-softmmu/qemu-system-arm" "../../qemu-system-arm"
+cp -f "work/qemu-2.3.0/aarch64-softmmu/qemu-system-aarch64" "../../qemu-system-aarch64"
